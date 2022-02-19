d9df774ab3d811f5b92aa381a81e2fb7d78e015f
GGuts111 <100003811+GGuts111@users.noreply.github.com>
2022-02-18 21:43:38 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,9 @@ export default function Home() {
         <Header title="D3 Baseball 2022 season" />
         <p className="description">
           The rankings are  
-<a href="https://www.suseagulls.com/sports/bsb/index">1. Salisbury</a>
-
+<br><a href="https://www.suseagulls.com/sports/bsb/index"> 1. Salisbury</a>
+<br><a href="https://www.cortlandreddragons.com/sports/baseball"> 2. Cortland</a>
+<br><a href="https://pioneers.marietta.edu/sports/baseball"> 3. Marietta</a>
         </p>
     
       </main>
