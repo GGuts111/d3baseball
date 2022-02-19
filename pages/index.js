18c5a79f48725af5a2632a2b1033558604837e3a
GGuts111 <100003811+GGuts111@users.noreply.github.com>
2022-02-18 21:26:27 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,9 @@ export default function Home() {
       <main>
         <Header title="D3 Baseball 2022 season" />
         <p className="description">
-          The rankings are \ln 1. Salisbury 
+          The rankings are  1. Salisbury 
         </p>
+    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thegazette.com%2Fsports%2Fsalisbury-university-sweeps-its-way-to-first-ncaa-division-iii-baseball-national-title%2F&psig=AOvVaw0P3bolkCu81M4H5nxUNuba&ust=1645323857204000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCPjAw5PbivYCFQAAAAAdAAAAABAw" alt="Salisbury" width="460" height="345">
       </main>
 
       <Footer />
