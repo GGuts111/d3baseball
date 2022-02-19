16fef1aee8ffe56ee68863d5ddb7650bcef4fe39
GGuts111 <100003811+GGuts111@users.noreply.github.com>
2022-02-18 21:38:39 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,11 @@ export default function Home() {
       <main>
         <Header title="D3 Baseball 2022 season" />
         <p className="description">
-          The rankings are  1. Salisbury 
+          The rankings are  
+<a href="https://www.suseagulls.com/sports/bsb/index">1. Salisbury</a>
+
         </p>
-    <img src="https://images.app.goo.gl/Th7u1TK8uKhYNbRz5" alt="Salisbury" width="20" height="20">
+    
       </main>
 
       <Footer />
